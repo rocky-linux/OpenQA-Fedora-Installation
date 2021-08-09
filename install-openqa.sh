fbc1220cd545469ad20b4dcf843088fa3fb0bbc6
Trevor Cooper <tcooper@rockylinux.org>
2021-08-09 01:12:46 -0700

    update install-openqa.sh to make it re-runnable

--- install-openqa.sh
+++ install-openqa.sh
@@ -1,25 +1,43 @@
 #!/bin/bash
 
+set -e
 
-# Test scrip based on installation guide from Fedora:
+# Test script based on installation guide from Fedora:
 # https://fedoraproject.org/wiki/OpenQA_direct_installation_guide
-export release=$(uname -a)
-echo Installing OpenQA on Fedora 
-echo Running on: $release
 
-#sudo dnf update -y 
-sudo dnf install -y git vim openssh-server openqa openqa-httpd openqa-worker fedora-messaging libguestfs-tools libguestfs-xfs python3-fedfind python3-libguestfs libvirt-daemon-config-network libvirt-python3 virt-install withlock postgresql-server fedora-messaging perl-REST-Client
+release=$(uname -a)
+export release
+echo Installing OpenQA on Fedora
+echo Running on: "$release"
 
-cd /etc/httpd/conf.d/
-sudo cp openqa.conf.template openqa.conf
-sudo cp openqa-ssl.conf.template openqa-ssl.conf
+pkgs=(git vim-enhanced openqa openqa-httpd openqa-worker fedora-messaging guestfs-tools libguestfs-xfs python3-fedfind python3-libguestfs libvirt-daemon-config-network virt-install withlock postgresql-server perl-REST-Client)
+if ! rpm -q "${pkgs[@]}" &> /dev/null; then
+  sudo dnf install -y "${pkgs[@]}"
+else
+  echo "openqa and all requirements installed."
+fi
 
-sudo cat << EOF >  /etc/openqa/openqa.ini 
+conf_count=$(find /etc/httpd/conf.d -name "openqa*.conf" | wc -l)
+if [[ ${conf_count} -ne 2 ]]; then
+  sudo cp /etc/httpd/conf.d/openqa.conf.template /etc/httpd/conf.d/openqa.conf
+  sudo cp /etc/httpd/conf.d/openqa-ssl.conf.template /etc/httpd/conf.d/openqa-ssl.conf
+else
+  echo "apache conf files for openqa exist."
+fi
+
+if [[ ! -f /etc/openqa/openqa.ini.orig ]]; then
+  sudo cp /etc/openqa/openqa.ini /etc/openqa/openqa.ini.orig
+  sudo touch -r /etc/openqa/openqa.ini /etc/openqa/openqa.ini.orig
+fi
+
+sudo bash -c "cat >/etc/openqa/openqa.ini <<'EOF'
 [global]
 branding=plain
-download_domains = fedoraproject.org
+download_domains = rockylinux.org fedoraproject.org opensuse.org
+
 [auth]
 method = Fake
+<<<<<<< Updated upstream
 EOF
 
 postgresql-setup --initdb
@@ -40,13 +58,55 @@ sudo systemctl restart httpd
 
 echo Note! the api key will expire in one day after installation!
 
-sudo cat << EOF >  /etc/openqa/client.conf 
+sudo cat << EOF >  /etc/openqa/client.conf
+=======
+EOF"
+
+if ! systemctl is-active postgresql.service &> /dev/null; then
+#ret=$?
+#if [[ $ret -ne 0 ]]; then
+  sudo postgresql-setup --initdb
+  sudo systemctl start postgresql
+  sudo systemctl enable postgresql
+fi
+
+if ! systemctl is-active sshd.service &> /dev/null; then
+#ret=$?
+#if [[ $ret -ne 0 ]]; then
+  sudo systemctl start sshd
+  sudo systemctl enable sshd
+fi
+
+systemctl is-active httpd.service &> /dev/null
+ret=$?
+if [[ $ret -ne 0 ]]; then
+  sudo systemctl start httpd
+  sudo systemctl start openqa-gru
+  sudo systemctl start openqa-scheduler
+  sudo systemctl start openqa-websockets
+  sudo systemctl start openqa-webui
+  sudo systemctl start fm-consumer@fedora_openqa_scheduler
+  sudo systemctl enable httpd
+  sudo systemctl enable openqa-gru
+  sudo systemctl enable openqa-scheduler
+  sudo systemctl enable openqa-websockets
+  sudo systemctl enable openqa-webui
+  sudo systemctl enable fm-consumer@fedora_openqa_scheduler
+  sudo setsebool -P httpd_can_network_connect 1
+  sudo systemctl restart httpd
+fi
+
+#sudo firewall-cmd --add-port=80/tcp
+#sudo firewall-cmd --runtime-to-permanent
+
+if sudo grep -q foo /etc/openqa/client.conf; then
+  sudo bash -c "cat >/etc/openqa/client.conf <<'EOF'
+>>>>>>> Stashed changes
 [localhost]
-key = 1234567890ABCDEF 
-secret = 1234567890ABCDEF 
-EOF
+key = 1234567890ABCDEF
+secret = 1234567890ABCDEF
+EOF"
+  echo "Note! the api key will expire in one day after installation!"
+fi
 
 echo "Done, preparations. Now log in one time! Then run sudo ./install-openqa-post.sh"
-
-
-
