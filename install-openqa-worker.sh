be7325bdb29df24f43ef0ef2491cc82e6c03a8a9
Trevor Cooper <tcooper@sdsc.edu>
2022-06-29 15:50:10 -0700

    Fixes for PR 18 (#1)
    * make split script executable
    
    * install openqa-worker package
    
    * exit if argument supplied
    
    * allow rerun with existing openqa-vnc service definition in firewalld
    
    * when openqa-workers.conf doesn't exist grep returns -2
    
    * correct name for openqa-worker@1.service

--- install-openqa-worker.sh
+++ install-openqa-worker.sh
@@ -5,9 +5,10 @@ set -e
 # Test script based on installation guide from Fedora:
 # https://fedoraproject.org/wiki/OpenQA_direct_installation_guide
 
-if [ -z "$1" ]
-  then
-    echo "No argument supplied, please enter OpenQA Server fqdn"
+if [ -z "$1" ]; then
+  echo "No argument supplied, please enter OpenQA Server fqdn, or if installing"
+  echo "server and worker on a single node supply 'localhost'."
+  exit 1
 fi
 
 
@@ -16,7 +17,7 @@ export release
 echo Installing OpenQA Worker on Fedora
 echo Running on: "$release"
 
-pkgs=(guestfs-tools libguestfs-xfs libvirt-daemon-config-network perl-REST-Client python3-libguestfs virt-install withlock)
+pkgs=(guestfs-tools libguestfs-xfs libvirt-daemon-config-network openqa-worker perl-REST-Client python3-libguestfs virt-install withlock)
 if ! rpm -q "${pkgs[@]}" &> /dev/null; then
   sudo dnf install -y "${pkgs[@]}"
 else
@@ -24,13 +25,15 @@ else
 fi
 
 # Open vnc port for 4 local worker clients
-sudo firewall-cmd --permanent --new-service=openqa-vnc
-sudo firewall-cmd --permanent --service=openqa-vnc --add-port=5991-5999/tcp
-sudo firewall-cmd --permanent --add-service openqa-vnc
-sudo firewall-cmd --permanent --new-service=openqa-socket
-sudo firewall-cmd --permanent --service=openqa-socket --add-port=20000-20089/tcp
-sudo firewall-cmd --permanent --add-service openqa-socket
-sudo firewall-cmd --reload
+if ! ( sudo firewall-cmd --get-services | grep -q openqa-vnc ); then
+  sudo firewall-cmd --permanent --new-service=openqa-vnc
+  sudo firewall-cmd --permanent --service=openqa-vnc --add-port=5991-5999/tcp
+  sudo firewall-cmd --permanent --add-service openqa-vnc
+  sudo firewall-cmd --permanent --new-service=openqa-socket
+  sudo firewall-cmd --permanent --service=openqa-socket --add-port=20000-20089/tcp
+  sudo firewall-cmd --permanent --add-service openqa-socket
+  sudo firewall-cmd --reload
+fi
 
 if sudo grep -q foo /etc/openqa/client.conf; then
   sudo bash -c "cat >/etc/openqa/client.conf <<'EOF'
@@ -41,13 +44,13 @@ EOF"
   echo "Note! the api key will expire in one day after installation!"
 fi
 
-if sudo grep -q http://openqa.example.host /etc/openqa/workers.conf; then
+if ! sudo grep -q http://openqa.example.host /etc/openqa/workers.conf; then
   sudo bash -c "cat >/etc/openqa/workers.conf <<'EOF'
 [global]
 HOST = https://$1
 EOF"
 fi
 
-if ! systemctl is-active openqa-worker.service@1 &> /dev/null; then
-  sudo systemctl enable --now openqa-worker.service@1
+if ! systemctl is-active openqa-worker@1.service &> /dev/null; then
+  sudo systemctl enable --now openqa-worker@1.service
 fi
